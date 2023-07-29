547a05ae1a0481200a31494d23aa65456a3b02d2
Diarmuid Lloyd <diarmuid.lloyd@gmail.com>
2023-07-29 12:39:51 +0100

    Initial commit (incomplete)

---
+++ 2023/2023-07-25/2023-07-25_scurvy.R
@@ -0,0 +1,20 @@
+library(tidyverse)
+
+scurvy <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2023/2023-07-25/scurvy.csv')
+
+
+glimpse(scurvy)
+
+scurvy_long <- scurvy |> 
+pivot_longer(cols = gum_rot_d6:lassitude_d6, values_to = "score", names_to = "ailment")
+
+# Convert scores into ordered factors
+# 
+
+scurvy_long$score <- forcats::as_factor(score)
+
+
+scurvy_long |> 
+  ggplot() +
+  geom_bar(aes(ailment, fill = score)) +
+  facet_grid(~treatment)
